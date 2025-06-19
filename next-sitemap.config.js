928424faa0289f9def8eba8a798e31961a468caf
nolandex <stemapal1234@gmail.com>
2025-06-20 00:45:16 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   siteUrl:
-    process.env.NEXT_PUBLIC_SITE_URL || "https://landingpage.weijunext.com",
+    process.env.NEXT_PUBLIC_SITE_URL || "https://bisnovo.my.id
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
